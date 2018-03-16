a42bae93822cf41461d590cef7b9f61808d544da
Mats Wikmar <matwik@gmail.com>
2018-03-16 21:48:31 +0100

    getting started with asp.net core in vs2017

---
+++ FirstAspNetCoreApp/FirstAspNetCoreApp/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace FirstAspNetCoreApp
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(
+          IApplicationBuilder app,
+          IHostingEnvironment env,
+          ILoggerFactory loggerFactory)
+        {
+
+      //loggerFactory.AddConsole();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      // install nuget staticfiles pkg and add this code.. so we can serve etc. index.html from: wwwroot
+      // the order matters.. we dont want to add this code after the throw error block etc
+      app.UseDefaultFiles();
+      app.UseStaticFiles();
+
+      app.Run((context) =>
+      {
+        // await context.Response.WriteAsync("Hello World! 123");
+        throw new Exception(); // it is not async.. so RM async keyword from fn
+      });
+    }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Visual Studio, ASP.NET examples
+##### Author: Mats Wikmar, mow-github, matwik@gmail.com
+
+![bviking](img/bVikingFullBlack-logo-150x150.png)
+
+##### Technologies etc:
+* Visual Studio 2017, ASP.NET, C# etc
+
+###### Installation example:
+<pre>git clone https://github.com/mow-github/vs-aspnet.git</pre>
+
+###### References:
+* [Getting Started with ASP.NET Core in Visual Studio 2017 (Web 101)](https://www.youtube.com/watch?v=yDp-sHGKTWo)
+
+<hr>
+
+> _"Failure is just a symptom for success"_
+
+`          | Links          | My webpage                              |
+---------- | -------------- | --------------------------------------- |
+*Author(s)*| `Mats Wikmar`  | [bviking.se](https://www.bviking.se)    |
