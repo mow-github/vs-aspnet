ecdec02b70091322207f3e4355e77f5920934da4
Mats Wikmar <matwik@gmail.com>
2018-03-17 12:42:10 +0100

    sqldataaccessDemo started, and numberguesser done

---
+++ NumberGuesser/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace NumberGuesser
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      getAppInfo();
+      string inputName = greetUser();
+
+
+
+      while (true) { 
+
+        int guess = 0;
+
+        Random random = new Random();
+        int correctNumber = random.Next(1, 10);
+
+        Console.WriteLine("Guess a number 1-10");
+
+        while (guess != correctNumber)
+        {
+          string input = Console.ReadLine();
+
+          if (!int.TryParse(input, out guess))
+          {
+            printColorMessage(ConsoleColor.Red, "-- You must enter a number --");
+            continue;
+          }
+
+
+          guess = Int32.Parse(input);
+
+          if (guess != correctNumber)
+          {
+            printColorMessage(ConsoleColor.Red, "Wrong number please try again...");
+          }
+
+        }
+
+        printColorMessage(ConsoleColor.Yellow, "Well played");
+
+        Console.WriteLine("Play again? [Y or N]");
+        string answer = Console.ReadLine().ToUpper();
+
+        if (answer == "Y")
+        {
+          continue;
+        }
+        else if (answer == "N")
+        {
+          return;
+        }
+
+      }
+
+    }
+
+
+    public static void getAppInfo()
+    {
+      string appName = "Number Guesser";
+      string appVersion = "1.0.0";
+      string appAuthor = "Brad Traversy";
+
+      string infoString = $"{appName}: Version {appVersion} by {appAuthor}";
+
+      printColorMessage(ConsoleColor.Green, infoString);
+    }
+
+    public static string greetUser()
+    {
+      Console.WriteLine("What is your name?");
+      string inputName = Console.ReadLine();
+      Console.WriteLine("Hello {0}, let's play a game...", inputName);
+      return inputName;
+    }
+
+    public static void printColorMessage(ConsoleColor color, string message)
+    {
+      Console.ForegroundColor = color;
+      Console.WriteLine(message);
+      Console.ResetColor();
+    }
+
+  }
+}
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # Visual Studio, ASP.NET examples
 ##### Author: Mats Wikmar, mow-github, matwik@gmail.com
 
-![bviking](img/bVikingFullBlack-logo-150x150.png)
+![bviking](_img/bVikingFullBlack-logo-150x150.png)
 
 ##### Technologies etc:
 * Visual Studio 2017, ASP.NET, C# etc
@@ -9,8 +9,15 @@
 ###### Installation example:
 <pre>git clone https://github.com/mow-github/vs-aspnet.git</pre>
 
+
+###### Projects:
+* 20180316 - FirstAspNetCoreApp, FirstAspNetCoreMvcApp  | [Getting Started with ASP.NET Core in Visual Studio 2017 (Web 101)](https://www.youtube.com/watch?v=yDp-sHGKTWo)
+* 20180317 - SQLDataAccessDemo | [How to connect C# to SQL (the easy way)](https://www.youtube.com/watch?v=Et2khGnrIqc)
+* 20180317 - NumberGuesser | [Build a C# .NET Application in 60 Minutes](https://www.youtube.com/watch?v=GcFJjpMFJvI)
+
+
 ###### References:
-* [Getting Started with ASP.NET Core in Visual Studio 2017 (Web 101)](https://www.youtube.com/watch?v=yDp-sHGKTWo)
+* asp.net links etc
 
 <hr>
 
