7ccec057fb943dd2c01a82aeb52fcbd5fad1c561
Mats Wikmar <matwik@gmail.com>
2018-03-24 22:56:19 +0100

    udemy aspnet mvc list mcache done

--- udemy_tuts/20180322_aspnet/05_42_mvc_ex4_mcache/05_42_mvc_ex4_mcache/05_42_mvc_ex4_mcache/Controllers/HomeController.cs
+++ udemy_tuts/20180322_aspnet/05_42_mvc_ex4_mcache/05_42_mvc_ex4_mcache/05_42_mvc_ex4_mcache/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Caching;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +10,24 @@ namespace _05_42_mvc_ex4_mcache.Controllers
 {
   public class HomeController : Controller
   {
+
+    ObjectCache cache = MemoryCache.Default;
+    List<Customer> customers;
+
+    public HomeController()
+    {
+      customers = cache["customers"] as List<Customer>;
+      if (customers == null)
+      {
+        customers = new List<Customer>();
+      }
+    }
+
+    public void SaveCache()
+    {
+      cache["customers"] = customers;
+    }
+
     public ActionResult Index()
     {
       return View();
@@ -51,12 +70,22 @@ namespace _05_42_mvc_ex4_mcache.Controllers
       return View();
     }
 
+    [HttpPost]
+    public ActionResult AddCustomer(Customer customer)
+    {
+      customer.Id = Guid.NewGuid().ToString();
+      customers.Add(customer);
+      SaveCache();
+
+      return RedirectToAction("CustomerList");
+    }
+
     public ActionResult CustomerList()
     {
 
-      List<Customer> customers = new List<Customer>();
-      customers.Add(new Customer() { Name = "Fred", Telephone = "21333" });
-      customers.Add(new Customer() { Name = "Moa", Telephone = "5634" });
+      //List<Customer> customers = new List<Customer>();
+      //customers.Add(new Customer() { Name = "Fred", Telephone = "21333" });
+      //customers.Add(new Customer() { Name = "Moa", Telephone = "5634" });
 
       return View(customers);
     }
