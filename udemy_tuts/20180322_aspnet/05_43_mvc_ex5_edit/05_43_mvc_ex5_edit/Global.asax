7206563356a2f9b96ec52f74dc5c534f0dd30791
Mats Wikmar <matwik@gmail.com>
2018-03-25 00:32:39 +0100

    udemy aspnet mvc edit done

---
+++ udemy_tuts/20180322_aspnet/05_43_mvc_ex5_edit/05_43_mvc_ex5_edit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_05_43_mvc_ex5_edit.MvcApplication" Language="C#" %>
---
+++ udemy_tuts/20180322_aspnet/05_43_mvc_ex5_edit/05_43_mvc_ex5_edit/Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _05_43_mvc_ex5_edit.Models
+{
+  public class Customer
+  {
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Telephone { get; set; }
+  }
+}
