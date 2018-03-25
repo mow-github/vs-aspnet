65c7ae11a28a4559039a879aa3a5b0ca99cebf2c
Mats Wikmar <matwik@gmail.com>
2018-03-25 21:24:33 +0200

    udemy aspnet auth init

---
+++ udemy_tuts/20180322_aspnet/06_49_auth/06_49_auth/06_49_auth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_06_49_auth.Startup))]
+namespace _06_49_auth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
