8ed7ae2f420d928e71e14c4d692aa1034d6b0f6a
Mats Wikmar <matwik@gmail.com>
2018-03-30 19:03:29 +0200

    udemy aspnet myshop project 09_73 Basket init done

---
+++ udemy_tuts/20180322_aspnet/08_58_project/MyShop/MyShop.Core/Models/Basket.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MyShop.Core.Models
+{
+  public class Basket : BaseEntity
+  {
+    // virtual keyword: good for MS Entity Framework.. so basket and basketitems request works together
+
+    public virtual ICollection<BasketItem> BasketItems { get; set; }
+
+    public Basket()
+    {
+      this.BasketItems = new List<BasketItem>();
+    }
+
+  }
+}
--- udemy_tuts/20180322_aspnet/08_58_project/MyShop/MyShop.DataAccess.SQL/DataContext.cs
+++ udemy_tuts/20180322_aspnet/08_58_project/MyShop/MyShop.DataAccess.SQL/DataContext.cs
@@ -1,10 +1,5 @@
 ﻿using MyShop.Core.Models;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyShop.DataAccess.SQL
 {
@@ -15,8 +10,10 @@ namespace MyShop.DataAccess.SQL
 
     }
 
-    public DbSet<Product> Products { get; set;  }
-    public DbSet<ProductCategory> ProductCategories { get; set;  }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<ProductCategory> ProductCategories { get; set; }
+    public DbSet<Basket> Baskets { get; set; }
+    public DbSet<BasketItem> BasketItems { get; set; }
 
   }
 }
