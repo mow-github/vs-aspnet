67f82703797afa7bacf5d15e7f6249c5db16063b
Mats Wikmar <matwik@gmail.com>
2018-04-01 20:22:53 +0200

    udemy aspnet myshop project 09_75 Basket p3 baskerservice done

---
+++ udemy_tuts/20180322_aspnet/08_58_project/MyShop/MyShop.Core/ViewModels/BasketItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace MyShop.Core.ViewModels
+{
+  public class BasketItemViewModel
+  {
+    // We are gonna inner join: product and basketItem model into this structure..
+    // we need etc the price * quantity = sum AND productname => display for the user
+
+    public string Id { get; set; }
+    public int Quantity { get; set; }
+    public string ProductName { get; set; }
+    public decimal Price { get; set; }
+    public string Image { get; set; }
+  }
+}
